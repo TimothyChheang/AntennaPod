442503b1cfd49d6c24800bdf562e25183d62114d
Ithyx <lamidey.m@gmail.com>
2022-02-14 15:31:27 -0500

    Revert "Ajout des tests unitaires"
    This reverts commit 0bbb83a723281c3babe577873206a1a0e3cd3cde.

--- StringManip/src/main/java/com/polytechnique/stringManip/StringManip.java
+++ StringManip/src/main/java/com/polytechnique/stringManip/StringManip.java
@@ -4,10 +4,12 @@ import android.util.Log;
 
 public class StringManip {
     public static String getHash(String string) {
+        Log.e("IMPORTANT STRINGMANIP", "Called !");
         return String.valueOf(string.hashCode());
     }
 
     public static String getReverse(String string) {
+        Log.e("IMPORTANT STRINGMANIP", "Also Called !");
         return new StringBuilder(string).reverse().toString();
     }
 }
--- StringManip/src/test/java/com/polytechnique/stringManip/ExampleUnitTest.java
+++ StringManip/src/test/java/com/polytechnique/stringManip/ExampleUnitTest.java
@@ -11,15 +11,7 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void reverse_isCorrect() {
-        String palindrome = "are we not pure no sir panamas moody noriega brags it is garbage irony dooms a man a prisoner up to new era".replaceAll(" ", "");
-        assertEquals(StringManip.getReverse(palindrome), palindrome);
-    }
-
-    @Test
-    public void hash_isCorrect() {
-        String string = "123456789";
-        String hash = String.valueOf(string.hashCode());
-        assertEquals(StringManip.getHash(string), hash);
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
     }
 }
